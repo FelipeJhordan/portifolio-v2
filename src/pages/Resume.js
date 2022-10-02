55a41e82660cf3f55410bef8e9bf9614a63f12a3
FelipeJhordan <felipejordan.alves@gmail.com>
2022-10-01 21:16:48 -0300

    feat: update my resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -107,7 +107,7 @@ const Resume = () => {
         </div>
       </div>
       <div className="flex flex-row justify-center m-5">
-      <a href={require("../assets/curriculo/cv-backend-felipe-atualizado.pdf")} download="cv-backend-felipe_atualizado" className="hover:text-gray-400	text-grey-darkest">
+      <a href={require("../assets/curriculo/cv-backend-felipe_atualizado.pdf")} download="cv-backend-felipe_atualizado" className="hover:text-gray-400	text-grey-darkest">
       <button className="bg-grey-light text-gray-400	hover:text-slate-100		 hover:bg-light text-gray-400  font-bold py-2 px-4 rounded inline-flex items-center">
   <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
   <span className="">{resume.download}</span>
